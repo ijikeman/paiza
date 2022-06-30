bb7023d4b89dcdb0c0fe9d6d22376918968af5d1
ijikeman <keiji.ue@gmail.com>
2022-07-01 00:52:00 +0900

    wip

---
+++ B/81/B81.php
@@ -0,0 +1,32 @@
+<?php
+class Stdin {
+    function get() {
+        if ($result = fgets(STDIN)) {
+            $result = trim($result);
+        }
+        return $result;
+    }
+}
+
+class Stdins extends Stdin {
+    function __construct($max_num) {
+        $this->max_num = $max_num; // 最大取得数を格納
+        $this->list = array(); // 取得した値を格納
+
+        for ($i=0; $i < $this->max_num; $i++) {
+            $result = parent::get();
+            if ($result == FALSE) {
+                break;
+            } else {
+                array_push($this->list, $result);
+            }
+        }
+    }
+}
+
+$first_line = new Stdins(1);
+list($max_tate, $max_yoko) = explode(' ', $first_line->list[0]);
+
+$data_master = new Stdins($max_tate);
+print_r($data_master->list);
+?>
