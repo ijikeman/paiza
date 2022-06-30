48d72aadad3dde2fcc0e4a7248443afc93dec677
ijikeman <keiji.ue@gmail.com>
2022-06-30 22:10:47 +0900

    add B108

--- B/108/B108.php
+++ B/108/B108.php
@@ -10,15 +10,18 @@ list ($MAX_GONDRA, $MAX_GROUP) = explode(' ', trim(fgets(STDIN)));
 for ($i=0; $i < $MAX_GONDRA; $i++) {
     $BOARDING_LIST[$i] = 0;
 }
+/* debug 
+print_r($BOARDING_LIST);
+*/
 
 $index = 0;
 while (($line = fgets(STDIN)) !== false ) {
     if ($index < $MAX_GONDRA) {
         // ゴンドラリストを作成
-        array_push($GONDRA_LIST, $line);
+        array_push($GONDRA_LIST, trim($line));
     } else {
         // 順番待ちリストを作成
-        array_push($MEMBER_LIST, $line);
+        array_push($MEMBER_LIST, trim($line));
     }
     $index++;
 }
@@ -32,23 +35,23 @@ $num = 0; // グループ人数
 
 // グループ人数を取得、全グループ搭乗すれば終了
 while ((int)$num = array_shift($MEMBER_LIST)) {
+    /* debug
+    print "member_num = $num";
+    */
     // 余りの人数を初期化する
     $amari = 0;
     // カウントアップ値を初期化
     $count_up = 0;
-
     // もしゴンドラ番号が最大なら初期化する
     if ($MAX_GONDRA <= $gondra_index) {
         $gondra_index = 0;
     }
-
     // ゴンドラを回して乗車人数を取得
     while ((int)$max_num = array_slice($GONDRA_LIST, $gondra_index, 1)[0]) {
         // もし余りの人数が0じゃなければ乗車人数として割り当てる
         if ($amari != 0) {
             $num = $amari;
         }
-
         // もし乗車人数が全員乗車できない場合
         if ($max_num < $num) {
             // カウントをMAX人数分にする
@@ -64,9 +67,13 @@ while ((int)$num = array_shift($MEMBER_LIST)) {
         $BOARDING_LIST[$gondra_index] += (int)$count_up;
         // ゴンドラ番号を次へ
         $gondra_index++;
-/* debug
-        print_r($BOARDING_LIST);
-*/
+        // もしゴンドラ番号が最大なら初期化する
+        if ($MAX_GONDRA <= $gondra_index) {
+            $gondra_index = 0;
+        }
+        /* debug
+                print_r($BOARDING_LIST);
+        */
         // 全員乗車できたので次のグループへ
         if ($amari == 0) {
             break;
@@ -75,7 +82,7 @@ while ((int)$num = array_shift($MEMBER_LIST)) {
 }
 
 // ゴンドラ毎の乗車人数を出力する
-while ($num = array_shift($BOARDING_LIST)) {
-    echo $num . "\n";
+for ($i=0; $i < count($BOARDING_LIST); $i++) {
+    print "$BOARDING_LIST[$i]\n";
 }
 ?>
