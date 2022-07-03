3edbcad7976d3867f867c1291576e06c4be608c1
ijikeman <keiji.ue@gmail.com>
2022-07-04 05:53:13 +0900

    wip

--- B/81/B81.php
+++ B/81/B81.php
@@ -7,7 +7,7 @@
  *   - string or FALSE
  */
 class Stdin {
-    function get() {
+    static function get() {
         if ($result = fgets(STDIN)) {
             $result = trim($result);
             return $result;
@@ -24,14 +24,11 @@ class Stdin {
  * @return
  *   - array()
  */
-class Stdins extends Stdin {
-    function __construct($max_num) {
-        $this->max_num = $max_num;
-    }
-    function get() {
+class Stdins {
+    static function get($max_num) {
         $list = array();
-        for ($i=0; $i < $this->max_num; $i++) {
-            $result = parent::get();
+        for ($i=0; $i < $max_num; $i++) {
+            $result = Stdin::get();
             if ($result == FALSE) {
                 break;
             } else {
@@ -41,6 +38,49 @@ class Stdins extends Stdin {
         return $list;
     }
 }
+
+/**
+ * Get 1 Line from File
+ *
+ * @method get()
+ * @return
+ *   - string or FALSE
+ */
+class GetLineFromFile {
+    static function get($filename) {
+        $fp = fopen($filename, 'r');
+        if ($result = fgets($fp)) {
+            $result = trim($result);
+            return $result;
+        }
+        return FALSE;
+    }
+}
+
+/** 
+ * Get Multi Line from File
+ *
+ * @method get()
+ * @param int $filename: ファイル名
+ * @param int $max_num: 最大取得可能行
+ * @return
+ *   - array()
+ */
+class GetLinesFromFile {
+    static function get($filename, $max_num) {
+        $list = array();
+        for ($i=0; $i < $max_num; $i++) {
+            $result = GetLineFromfile::get($filename);
+            if ($result == FALSE) {
+                break;
+            } else {
+                array_push($list, $result);
+            }
+        }
+        return $list;
+    }
+}
+
 /**
  * Set Data of Paiza-format to Array
  *
@@ -48,89 +88,83 @@ class Stdins extends Stdin {
  * @method getData()
  * @param int $max_num
  */
-class PaizaData extends Stdins {
+class PaizaData {
     function __construct() {
         $this->data = array();
     }
-    function setData($max_num) {
-        parent::__construct($max_num);
-        $this->data[] = parent::get();
+    function setData($data) {
+        $this->data[] = $data;
     }
     function getData($array_num) {
-        return $this->data[$array_num];
+        return $this->data[($array_num-1)];
     }
 }
 
 /**
  * 判定した数値を返す
  */
-class JudgeB81
-    $SCORE = 0;
-    function judge($current_data, $before_data, $before_line_data) {
+class CountScore {
+    public $SCORE = 0;
+    function judge($current_data, $before_data=null, $before_line_data=null, $after_line_data=null) {
         $this->current_data = $current_data;
         $this->before_data = $before_data;
         $this->before_line_data = $before_line_data;
-        return $SCORE;
     }
-
+}
 
 /**
  *  main
  */
 $paiza = new PaizaData();
-$paiza->setData(1); // get first line
-list($tate_max, $yoko_max) = explode(' ', trim($paiza->getData(0)[0])); // parse First Line
-$paiza->setData($tate_max); // データを設置
+list($tate_max, $yoko_max) = explode(' ', trim(Stdin::get(1))); // parse First Line
+$paiza->setData(Stdins::get($tate_max)); // set Second Data
 print_r($paiza->data);
 
-/* データ分析開始 */
-
-
-/*
-foreach (explode(' ', trim($paiza->getData(0)[0])) as $i) { // parse First Line
-    $paiza->setData($i);
-    print_r($paiza->data);
+$split_data = array(); // コンテンツデータを１文字ずつ分離して格納する
+for ($i=2; $i <= $tate_max; $i++) {
+    $split_data[] = str_split($paiza->getData($i));
+    print_r($split_data);
 }
-// 条件
-/* -- 全ての行で判定する
-// 前がなしで次が. 0
-// 前がなしで次が# +1
-// 前が.で次が# +1
-// 前が.で次が. 0
-// 前が#で次が# 0
-// 前が#で次が. +1
 
-/* -- 最初の行と最後の行は判定する
-// 最初/最終行.なら 0
-// 最初/最終行#なら +1
-
-/* -- 最初の行以外は判定する
-// 前の行がなしで.0
-// 前の行がなしで# +1
-// 前の行が.で. 0
-// 前の行が#で. +1
-// 前の行が.で# +1
-// 前の行が#で # 0
-
-/* --判定条件終了 --
-
-.##.
-.##.
-.##.
-.##.
+/**
+ * データ分析開始
+ * 条件
+ * 全ての行で判定する 
+ * 前がなしで次が. 0
+ * 前がなしで次が# +1
+ * 前が.で次が# +1
+ * 前が.で次が. 0
+ * 前が#で次が# 0
+ * 前が#で次が. +1
+ * -- 最初の行と最後の行は判定する
+ * 最初/最終行.なら 0
+ * 最初/最終行#なら +1
+ * -- 最初の行以外は判定する
+ * 前の行がなしで.0
+ * 前の行がなしで# +1
+ * 前の行が.で. 0
+ * 前の行が#で. +1
+ * 前の行が.で# +1
+ * 前の行が#で # 0
+ * --判定条件終了 --
+ */
+// .##.
+// .##.
+// .##.
+// .##.
 // 0211 4
 // 0101 2
 // 0101 2
 // 0211 4
-=12
+//=12
 
-..#.
-..#.
-###.
-..#.
+// ..#.
+// ..#.
+// ###.
+// ..#.
 // 0021
 // 0011
 // 2101
 // 1121
-=14
+// =14
 ?>
