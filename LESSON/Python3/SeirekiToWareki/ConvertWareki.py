76db7a66654833c27d2d46e1067ebb1c0e0c63ac
keiji.uehata <keiji.uehata@rakus.co.jp>
2022-07-11 14:33:05 +0900

    fixed wareki

--- LESSON/Python3/SeirekiToWareki/ConvertWareki.py
+++ LESSON/Python3/SeirekiToWareki/ConvertWareki.py
@@ -1,14 +1,12 @@
 import sys
 
-class Validation:
-    # intかどうか
-    @staticmethod
-    def CheckInt(param):
-        return isinstance(param, int)
-
 class ConvertWareki:
+    """
+    渡された西暦年から、計算用キーとなる西暦と和暦元号を返す
+    return: @計算用西暦, 和暦元号
+    """
     @staticmethod
-    def __GetKeyYear(year):
+    def __GetParameter(year):
         if 2019 < year:
             return [2018, "令和"]
         elif 1989 < year:
@@ -16,18 +14,20 @@ class ConvertWareki:
         elif 1926 < year:
             return [1925, "昭和"]
 
+    """
+    和暦の年数を計算する
+    """
     @classmethod
-    def Convert(cls, year): # GetKeyYearを呼ぶ為にクラスメソッドにしている
-        if Validation.CheckInt(year):
-            key_year, key_wareki = cls.__GetKeyYear(year)
-            if (year < key_year):
-                raise (ValueError)
-            else:
-                return [year - key_year, key_wareki]
-        else:
-            raise (TypeError)
+    def Convert(cls, year): # GetParameterを呼ぶ為にクラスメソッドにしている
+        key_year, initial_wareki = cls.__GetParameter(year)
+        return [year - key_year, initial_wareki]
 
 try:
-    print(ConvertWareki.Convert(int(sys.argv[1])))
+    # int型に変換
+    input_year = int(sys.argv[1])
+    # 和暦を計算
+    convert_year, initial_wareki = ConvertWareki.Convert(input_year)
+    # 出力
+    print("西暦", str(input_year), "年は、" + str(initial_wareki) + str(convert_year) + "年です")
 except Exception as e:
-    print(e)
+    sys.stderr.write(e)
